2bc777cfac1e8c11f5acce89e577b7178375d08b
Janani S <Janani S@DESKTOP-13MT9AS>
2024-10-15 19:39:00 +0530

    Add project files.

---
+++ UnicodeEncoding/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnicodeEncoding
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string mytext = "\u0543";
+            Console.WriteLine(mytext);
+            string mytext2 = "Ɏ";
+            Console.WriteLine(mytext2);
+            Console.ReadKey();
+
+        }
+    }
+}
